0e65c0f3e46acdb621d2040f6dd53009a94f5640
ndremferraz <andreferraz@gwu.edu>
2026-05-06 12:26:22 -0400

    changed SLURM file

--- gpu_job_submission_template.sh
+++ gpu_job_submission_template.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash -l
 
 # Hello! Here is a template for submitting a GPU job via SLURM, while explaining the different options and lines.
 # This has been updated to accommodate the new resource-based scheduling system, and is accurate as of February 27th, 2026.
@@ -13,13 +13,13 @@
 # If your code has yet to finish by the time the value specified here has been hit, it will terminate prematurely.
 # Make sure to run tests of your code with small amounts of input to see how long it may run for, then extrapolate for the final.
 # Please keep in mind that you cannot set a time longer than the queue's timelimit. On Pegasus, this is 7 days for GPU jobs.
-SBATCH -t 4:00:00
+#SBATCH --time=04:00:00
 
 # -p stands for partition, or "queue" as we colloquially refer to them. These are a remnant of the old style of scheduling,
 # and are planned for decommissioning once we swap to the resource-based scheduling system full time on May 9th, 2026.
 # For now, however, you will still need to specify a queue in order to use the resource-based system.
 # For GPU jobs on Pegasus and Raptor, use 'gpu'. For all resource-based requests on Cerberus, use 'workshop'.
-SBATCH -p workshop
+#SBATCH --partition=workshop
 
 # The next options are also important, and you'll need to define all of them in order to make the most of the new system.
 # While there are defaults, you typically want to adjust the values for your code's particular needs and requirements.
@@ -45,7 +45,7 @@ SBATCH -p workshop
 # will not use more than one. Parallelization is also required for requesting more GPUs than available on a single node.
 # Thus, you should almost always put a "1" for this section of the command, unless you're certain your code can take advantage.
 # The aforementioned command should show you what's available at most on a single node, so use that to fill out this option.
-#SBATCH --gres=gpu:<TYPE>:<COUNT>
+#SBATCH --gres=gpu:1
 
 # --cpus-per-gpu are the number of cores you want for your job, per GPU requested.
 # SLURM often conflates the terms 'cpu' and 'core', so keep that in mind as you adjust these settings.
@@ -53,7 +53,7 @@ SBATCH -p workshop
 # we suggest that you request fewer than an entire node's worth of cores, unless your jobs require a high core count.
 # Different GPU nodes have different total core counts, so check using "scontrol show node [nodename]" for particulars. 
 # If unspecified, you will be given 4 cores total.
-SBATCH --cpus-per-gpu=4
+#SBATCH --cpus-per-gpu=4
 
 # --mem-per-gpu is the total amount of memory (RAM) you wish to have for your job, per GPU requested.
 # This is NOT VRAM. Rather, this is the total DRAM for your CPU cores. Use G for gigabytes, M for megabytes.
@@ -61,7 +61,7 @@ SBATCH --cpus-per-gpu=4
 # Should you prefer specifying the total overall memory for the entire job, feel free to use --mem instead.
 # Failure to specify a value will leave you with the default 4GB per core requested; thus, make sure to specify as much as you need.
 # At the same time, be reasonable with your requests. Higher amounts of requested RAM will likely take longer to allocate.
-SBATCH --mem-per-gpu=16G
+#SBATCH --mem-per-gpu=16G
 
 # We now move onto four options that, while not required, are highly recommended for ease of debugging your jobs and staying
 # informed about how your jobs are running.
@@ -71,14 +71,14 @@ SBATCH --mem-per-gpu=16G
 # Error contains any errors that appear while running the code; these traditionally appear in the terminal in red.
 # Using these two effectively can allow you to debug your code when attempting to run it on Pegasus for the first time.
 # Additionally, the "%j" portion of the filename will write out the job submission number of the job itself.
-SBATCH -o testing%j.out
-SBATCH -e testing%j.err
+#SBATCH --output=testing%j.out
+#SBATCH --error=testing%j.err
 
 # These final two options notify you via email of when your job has done any number of things; setting the latter to
 # "all" just makes it notify you of when it was submitted, when it begins to run, and when it either completes or fails.
 # Please put your username in the designated spot before submitting this, so you can get the emails yourself!
-#SBATCH --mail-user=[username]@gwu.edu
-#SBATCH --mail-type=all
+##SBATCH --mail-user=your_netid@gwu.edu
+##SBATCH --mail-type=all
 
 # That is the extent of the recommended / required SBATCH options we suggest for your submission script. Once all of these are
 # as you'd like, the section below without any pound symbols are the actual payload of your script; the code you'd like to run.
